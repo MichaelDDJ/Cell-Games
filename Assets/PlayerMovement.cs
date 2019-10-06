2cb73cbdbb83b1239888986eec2e621165b07e95
MichaelDDJ <55068644+MichaelDDJ@users.noreply.github.com>
2019-10-06 10:42:28 -0500

    Add files via upload

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -16,17 +16,28 @@ public class PlayerMovement : MonoBehaviour {
     }
 
 
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
 
         float moveForward = Input.GetAxis("Vertical") * speed;
         float Turn = Input.GetAxis("Horizontal") * -rotate;
 
+        if ((moveForward + thrust) >= 0.5)
+        { 
+
         moveForward *= Time.deltaTime;
         Turn *= Time.deltaTime;
 
-        transform.Translate(0,(thrust + moveForward), 0);
+        transform.Translate(0, (thrust + moveForward), 0);
         transform.Rotate(0, 0, Turn);
+
+            thrust = 0.01f;
+        }
+        else if (0.5 > (moveForward + thrust) && (moveForward + thrust) > 0)
+        {
+            thrust = thrust - 0.00005f;
+            transform.Translate(0, (thrust), 0);
+        }
 	}
 }
