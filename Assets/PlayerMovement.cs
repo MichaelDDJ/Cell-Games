fb5dc4d7882478f76936c28e8f6bc634cf9e5a0e
MichaelDDJ <55068644+MichaelDDJ@users.noreply.github.com>
2019-10-08 19:42:15 -0500

    Add files via upload

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    public float maxspeed;
     public float speed;
     public float thrust;
     public float rotate;
     public float hp;
+    
 
 
     // Use this for initialization
@@ -20,24 +22,30 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        float moveForward = Input.GetAxis("Vertical") * speed;
+
+        maxspeed = 3;
+        float moveForward = Input.GetAxis("Vertical") * maxspeed;
         float Turn = Input.GetAxis("Horizontal") * -rotate;
-
-        if ((moveForward + thrust) >= 0.5)
-        { 
-
-        moveForward *= Time.deltaTime;
         Turn *= Time.deltaTime;
-
-        transform.Translate(0, (thrust + moveForward), 0);
         transform.Rotate(0, 0, Turn);
+        thrust = 0.01f;
 
-            thrust = 0.01f;
+        if (moveForward > 0)
+        { 
+            
+            moveForward *= Time.deltaTime;
+            transform.Translate(0, (thrust + moveForward), 0);
+            
+            
+            moveForward = speed;
         }
-        else if (0.5 > (moveForward + thrust) && (moveForward + thrust) > 0)
+        else if (0 > moveForward)
         {
-            thrust = thrust - 0.00005f;
-            transform.Translate(0, (thrust), 0);
+            transform.Translate(0, 0, 0);
+        }
+        else if (moveForward == 0)
+        {
+            transform.Translate(0, thrust, 0);
         }
 	}
 }
